89537e728f238784da49894d6f6eb13128e1fef7
Samet UCA <sametuca@hotmail.com>
2020-09-27 18:13:38 +0300

    Initial commit

---
+++ HotelFinder.DataAccess/Concrete/HotelRepository.cs
@@ -0,0 +1,58 @@
+﻿using HotelFinder.DataAccess.Abstract;
+using HotelFinder.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HotelFinder.DataAccess.Concrete
+{
+    public class HotelRepository : IHotelRepository
+    {
+        public Hotel CreateHotel(Hotel hotel)
+        {
+            using (var useDbContext = new HotelDbContext())
+            {
+                useDbContext.Hotels.Add(hotel);
+                useDbContext.SaveChanges();
+                return hotel;
+            }
+        }
+
+        public void DeleteHotel(int id)
+        {
+            using (var useDbContext = new HotelDbContext())
+            {
+                var deleteHotel = GetHotelById(id);
+                useDbContext.Remove(deleteHotel);
+                useDbContext.SaveChanges();
+            }
+        }
+
+        public List<Hotel> GetAllHotels()
+        {
+            using (var useDbContext = new HotelDbContext())
+            {
+                return useDbContext.Hotels.ToList();
+            }
+        }
+
+        public Hotel GetHotelById(int id)
+        {
+            using (var useDbContext = new HotelDbContext())
+            {
+                return useDbContext.Hotels.Find(id);
+            }
+        }
+
+        public Hotel UpdateHotel(Hotel hotel)
+        {
+            using (var useDbContext = new HotelDbContext())
+            {
+                useDbContext.Hotels.Update(hotel);
+                useDbContext.SaveChanges();
+                return hotel;
+            }
+        }
+    }
+}
---
+++ HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,18 @@
+﻿using HotelFinder.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;Database=HotelsDb;Trusted_Connection=True;");
+        }
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
